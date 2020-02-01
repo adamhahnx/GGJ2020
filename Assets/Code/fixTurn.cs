d1a1380e9e41488b9c050a10e4cdcba8789485c3
Adam Hahn <ahahn@unomaha.edu>
2020-02-01 14:49:09 -0600

    added turning to valves

---
+++ Assets/Code/fixTurn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fixTurn : MonoBehaviour
+{
+    Camera cam;
+    Vector3 sc;
+
+    void Awake()
+    {
+      cam = GameObject.Find("camera").GetComponent<Camera>();
+    }
+
+    void FixedUpdate()
+    {
+        float x = cam.WorldToScreenPoint(transform.position).x / Screen.width;
+        float y = cam.WorldToScreenPoint(transform.position).y / Screen.height;
+        if (x < 0.55f && x > 0.45f && y < 0.55f && y > 0.45f)
+        {
+          transform.Rotate(transform.forward * Input.GetAxis("Mouse ScrollWheel") * 100f);
+        }
+    }
+}
