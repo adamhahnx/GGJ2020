a9f88fb0028ee6f8f1b51e12388a22ca7303d97d
Adam Hahn <ahahn@unomaha.edu>
2020-02-01 10:55:37 -0600

    removed occulus :(

---
+++ Assets/Code/jumpMan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpMan : MonoBehaviour
+{
+  float floor;
+  bool jumping = false;
+
+  void Awake()
+  {
+    floor = transform.position.y;
+  }
+
+  public void Jump()
+  {
+    jumping = true;
+    transform.position += new Vector3(0f, 1f, 0f);
+  }
+
+  void Update()
+  {
+    if(jumping)
+    {
+      transform.position = new Vector3(0f, Mathf.Lerp(transform.position.y, transform.position.y + 1f, Time.deltaTime), 0f);
+    }
+    else
+    {
+      transform.position = new Vector3(0f, Mathf.Lerp(transform.position.y, floor, Time.deltaTime), 0f);
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Fogie's Arcade Repair
+
+1920s style 'game within a game' in which you repair a mechanical (steampunk-esque) arcade while you're playing it.  
+Playable in any WebGL-enabled browser.  
+Developed in a weekend for #GGJ20  
+  
+Code - Adam Hahn https://adamhahn.app  
+Art - Oakland Elliff https://www.artstation.com/oakieland  
+  
+Music - Gathacol Radio https://archive.org/details/Free_20s_Jazz_Collection  
+Mechanical Sound Effect - dklon https://opengameart.org/content/broken-fan-loop
